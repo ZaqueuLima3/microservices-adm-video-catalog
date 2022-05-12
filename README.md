adde36ba394628cdd84087791508ca7feb63d66a
Zaqueu Santos <zaqueulima1@gmail.com>
2022-05-12 06:50:01 -0300

    test: ensure that UniqueEntityId create and validate uuid pased in constructor

---
+++ README.md
@@ -0,0 +1,5 @@
+## Start project
+- docker-compose up --build
+- docker-compose up -d
+
+- docker-compose exec app bash
--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -1,11 +1,32 @@
+import { validate } from "uuid";
 import InvalidUuidError from "../errors/invalid-uuid.error";
 import UniqueEntityId from "./unique-entity-id.vo";
 
-describe("UniqueEntityId Unit Tests", () =>
+function spyValidateMethod() {
+  return jest.spyOn(UniqueEntityId.prototype as any, "validate");
+}
+
+describe("UniqueEntityId Unit Tests", () => {
   it("should throw error when uuid is invalid", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const validateSpy = spyValidateMethod();
     expect(() => {
       new UniqueEntityId("invalid-id");
     }).toThrow(new InvalidUuidError());
     expect(validateSpy).toHaveBeenCalled();
-  }));
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = spyValidateMethod();
+    const uuid = "745ea3de-c65e-4557-bfe7-b486d5693e4e";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.value).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should create a uuid when do not sent one in constructor", () => {
+    const validateSpy = spyValidateMethod();
+    const vo = new UniqueEntityId();
+    expect(validate(vo.value)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
