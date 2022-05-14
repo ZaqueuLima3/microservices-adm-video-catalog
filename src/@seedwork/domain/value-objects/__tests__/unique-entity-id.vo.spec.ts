23f099e004e207b36663fbf3788666266b2f1648
Zaqueu Santos <zaqueulima1@gmail.com>
2022-05-14 00:38:53 +0000

    refactor: :truck: move vo test to a specific folder

--- src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,6 +1,6 @@
 import { validate } from "uuid";
-import InvalidUuidError from "../../errors/invalid-uuid.error";
-import UniqueEntityId from "./unique-entity-id.vo";
+import InvalidUuidError from "../../../errors/invalid-uuid.error";
+import UniqueEntityId from "./../unique-entity-id.vo";
 
 function spyValidateMethod() {
   return jest.spyOn(UniqueEntityId.prototype as any, "validate");
