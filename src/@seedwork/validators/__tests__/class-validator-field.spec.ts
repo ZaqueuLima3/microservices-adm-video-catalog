fcbc45d46640a85cfe954d2ff5ef99d0a5218e5a
Zaqueu Santos <zaqueulima1@gmail.com>
2022-05-24 11:01:24 +0000

    refactor: :recycle: move validator rules test to a separeted folder

---
+++ src/@seedwork/validators/__tests__/class-validator-field.spec.ts
@@ -0,0 +1,37 @@
+import ClassValidatorFields from "../class-validator-fields";
+import * as libClassValidator from "class-validator";
+
+class StubClassValidator extends ClassValidatorFields<{ field: string }> {}
+
+describe("Class validator field tests", () => {
+  it("should initialize error and validatedData with null", () => {
+    const validator = new StubClassValidator();
+    expect(validator.errors).toBeNull();
+    expect(validator.validateData).toBeNull();
+  });
+
+  it("should validate with errors", () => {
+    const validateSyncSpy = jest.spyOn(libClassValidator, "validateSync");
+    validateSyncSpy.mockReturnValue([
+      {
+        property: "field",
+        constraints: { isRequired: "some error" },
+      },
+    ]);
+    const validator = new StubClassValidator();
+    expect(validator.validate(null)).toBeFalsy();
+    expect(validateSyncSpy).toHaveBeenCalled();
+    expect(validator.validateData).toBeNull();
+    expect(validator.errors).toStrictEqual({ field: ["some error"] });
+  });
+
+  it("should validate without errors", () => {
+    const validateSyncSpy = jest.spyOn(libClassValidator, "validateSync");
+    validateSyncSpy.mockReturnValue([]);
+    const validator = new StubClassValidator();
+    expect(validator.validate({ field: "value" })).toBeTruthy();
+    expect(validateSyncSpy).toHaveBeenCalled();
+    expect(validator.validateData).toStrictEqual({ field: "value" });
+    expect(validator.errors).toBeNull();
+  });
+});
