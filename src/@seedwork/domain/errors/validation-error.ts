46b87fd4c556d3cf6dac56acfd7bda31610845e7
Zaqueu Santos <zaqueulima1@gmail.com>
2022-05-27 10:58:58 +0000

    feat: :sparkles: create EntityValidationError

--- src/@seedwork/domain/errors/validation-error.ts
+++ src/@seedwork/domain/errors/validation-error.ts
@@ -1,1 +1,10 @@
-export default class ValidationError extends Error {}
+import { FieldErrors } from "../validators/validator-fields-interface";
+
+export class ValidationError extends Error {}
+
+export class EntityValidationError extends Error {
+  constructor(public error: FieldErrors) {
+    super("Entity Validation Error");
+    this.name = "EntityValidationError";
+  }
+}
