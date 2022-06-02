aca6a9c1dcbfd6c5a04758721c8b14d291f3da2b
Zaqueu Santos <zaqueulima1@gmail.com>
2022-06-02 11:26:27 +0000

    feat: :sparkles: create in memory repository

---
+++ src/@seedwork/domain/repository/in-memory.repository.ts
@@ -0,0 +1,44 @@
+import Entity from "../enity/entity";
+import NotFoundError from "../errors/not-found.error";
+import UniqueEntityId from "../value-objects/unique-entity-id.vo";
+import { RepositoryInterface } from "./repostiroy-contracts";
+
+export default abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  async findById(id: string | UniqueEntityId): Promise<E> {
+    const _id = `${id}`;
+    return this._get(_id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+    const index = this.items.findIndex((ett) => ett.id === entity.id);
+    this.items[index] = entity;
+  }
+
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    const _id = `${id}`;
+    this._get(_id);
+    const index = this.items.findIndex((ett) => ett.id === _id);
+    this.items.splice(index, 1);
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const item = this.items.find((ett) => ett.id === id);
+    if (!item) {
+      throw new NotFoundError(`Entity Not Found using ID ${id}`);
+    }
+    return item;
+  }
+}
