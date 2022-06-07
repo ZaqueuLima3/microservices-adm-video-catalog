859fd30dc8ca41e71844d62ba0cbd4fca84af0f8
Zaqueu Santos <zaqueulima1@gmail.com>
2022-06-07 12:04:15 +0000

    refactor: :art: add Filter generic to SearchParams

--- src/@seedwork/domain/repository/repostiroy-contracts.ts
+++ src/@seedwork/domain/repository/repostiroy-contracts.ts
@@ -19,14 +19,14 @@ export type SearchProps<Filter = string> = {
   filter?: Filter | null;
 };
 
-export class SearchParams {
+export class SearchParams<Filter = string> {
   protected _page: number = 1;
   protected _per_page: number = 15;
   protected _sort: string = null;
   protected _sort_dir: SortDirection = null;
-  protected _filter: string = null;
+  protected _filter: Filter = null;
 
-  constructor(props: SearchProps = {}) {
+  constructor(props: SearchProps<Filter> = {}) {
     this.page = props.page;
     this.per_page = props.per_page;
     this.sort = props.sort;
@@ -87,12 +87,12 @@ export class SearchParams {
     this._sort_dir = dir !== "asc" && dir !== "desc" ? "asc" : dir;
   }
 
-  get filter(): string | null {
+  get filter(): Filter | null {
     return this._filter;
   }
 
-  private set filter(value: string | null) {
-    this._filter = this.isEmpty(value) ? null : `${value}`;
+  private set filter(value: Filter | null) {
+    this._filter = this.isEmpty(value) ? null : (`${value}` as any);
   }
 
   private isEmpty(value: any) {
