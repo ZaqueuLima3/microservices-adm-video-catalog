3ee729af30d889d040bb0821c43f07a6dc1e62a6
Zaqueu Santos <zaqueulima1@gmail.com>
2022-06-07 12:10:31 +0000

    refactor: :art: use SearchRepository Genric types in CategoryRepository

--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -1,5 +1,23 @@
-import { SearchableRepositoryInterface } from "../../../@seedwork/domain/repository/repostiroy-contracts";
+import {
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from "../../../@seedwork/domain/repository/repostiroy-contracts";
 import Category from "../entities/category";
 
+export type CategoryFilter = string;
+
+export class CategorySearchParams extends SearchParams<CategoryFilter> {}
+
+export class CategorySearchResult extends SearchResult<
+  Category,
+  CategoryFilter
+> {}
+
 export default interface CategoryRepository
-  extends SearchableRepositoryInterface<Category, any, any> {}
+  extends SearchableRepositoryInterface<
+    Category,
+    CategoryFilter,
+    CategorySearchParams,
+    CategorySearchResult
+  > {}
