0049e7f6d891685a0a04bcd284903af1a1c3d485
Zaqueu Santos <zaqueulima1@gmail.com>
2022-06-07 13:36:00 +0000

    feat: :art: implement applySort CategoryRepository method

--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -1,23 +1,24 @@
 import {
   SearchableRepositoryInterface,
-  SearchParams,
-  SearchResult,
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
 } from "../../../@seedwork/domain/repository/repostiroy-contracts";
 import Category from "../entities/category";
 
-export type CategoryFilter = string;
+namespace CategoryRepository {
+  export type Filter = string;
 
-export class CategorySearchParams extends SearchParams<CategoryFilter> {}
+  export class SearchParams extends DefaultSearchParams<Filter> {}
 
-export class CategorySearchResult extends SearchResult<
-  Category,
-  CategoryFilter
-> {}
+  export class SearchResult extends DefaultSearchResult<Category, Filter> {}
 
-export default interface CategoryRepository
-  extends SearchableRepositoryInterface<
-    Category,
-    CategoryFilter,
-    CategorySearchParams,
-    CategorySearchResult
-  > {}
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      Category,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {}
+}
+
+export default CategoryRepository;
--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,15 +1,37 @@
+import { SortDirection } from "@seedwork/domain/repository/repostiroy-contracts";
 import { InMemorySearchableRepository } from "../../../@seedwork/domain/repository/in-memory.repository";
 import Category from "../../domain/entities/category";
 import CategoryRepository from "../../domain/repository/category.repository";
 
 export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
-  implements CategoryRepository
+  implements CategoryRepository.Repository
 {
-  protected applyFilter(
+  searchableField: string[] = ["name", "created_at"];
+
+  protected applySort(
     items: Category[],
-    filter: string
+    sort: string,
+    sort_dir: SortDirection
   ): Promise<Category[]> {
-    return Promise.reject(() => [] as Category[]);
+    if (!sort) {
+      return super.applySort(items, "created_at", "desc");
+    }
+    return super.applySort(items, sort, sort_dir);
+  }
+
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return i.props.name
+        .toLocaleLowerCase()
+        .includes(filter.toLocaleLowerCase());
+    });
   }
 }
