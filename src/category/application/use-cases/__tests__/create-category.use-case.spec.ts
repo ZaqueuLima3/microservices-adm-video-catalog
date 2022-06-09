51605b65c2bbe7eebdf56622eddc8adc8460238c
Zaqueu Santos <zaqueulima1@gmail.com>
2022-06-09 12:24:29 +0000

    feat: :sparkles: create GetCategoryUseCase

--- src/category/application/use-cases/__tests__/create-category.use-case.spec.ts
+++ src/category/application/use-cases/__tests__/create-category.use-case.spec.ts
@@ -12,15 +12,17 @@ describe("CreateCategoryUseCase Unit Tests", () => {
   });
 
   it("should create a category", async () => {
+    const spyInsert = jest.spyOn(repository, "insert");
     let output = await createCategoryUseCase.execute({ name: "test" });
     let items = repository.items;
     expect(output).toStrictEqual({
-      id: items[0].id.toString(),
+      id: items[0].id,
       name: "test",
       description: null,
       is_active: true,
       created_at: items[0].created_at,
     });
+    expect(spyInsert).toHaveBeenCalledTimes(1);
 
     output = await createCategoryUseCase.execute({
       name: "test",
@@ -29,11 +31,12 @@ describe("CreateCategoryUseCase Unit Tests", () => {
     });
     items = repository.items;
     expect(output).toStrictEqual({
-      id: items[1].id.toString(),
+      id: items[1].id,
       name: "test",
       description: "some description",
       is_active: false,
       created_at: items[1].created_at,
     });
+    expect(spyInsert).toHaveBeenCalledTimes(2);
   });
 });
--- src/category/application/use-cases/get-category.use-case.ts
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -1,23 +1,19 @@
-import Category from "../../domain/entities/category";
 import { CategoryOutput } from "../dto/category-output.dto";
 import CategoryRepository from "./../../domain/repository/category.repository";
 
 export type Input = {
-  name: string;
-  description?: string;
-  is_active?: boolean;
+  id: string;
 };
 
 export type Output = CategoryOutput;
 
-export default class CreateCategoryUseCase {
+export default class GetCategoryUseCase {
   constructor(
     private readonly categoryRepository: CategoryRepository.Repository
   ) {}
 
   async execute(input: Input): Promise<Output> {
-    const entity = new Category(input);
-    await this.categoryRepository.insert(entity);
+    const entity = await this.categoryRepository.findById(input.id);
     return {
       id: entity.id,
       name: entity.name,
