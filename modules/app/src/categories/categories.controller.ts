6f12f3d75f5e9fea50053fc96881a9beaf189980
Zaqueu Santos <zaqueulima1@gmail.com>
2022-07-14 23:41:13 +0000

    feat: :sparkles: create category module

---
+++ modules/app/src/categories/categories.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Inject,
+  Put,
+  HttpCode,
+  Query,
+} from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import {
+  CreateCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+} from '@micro-videos/core/category/application';
+import { SearchCategoryDto } from './dto/search-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  @Inject(CreateCategoryUseCase.UseCase)
+  private readonly createUseCase: CreateCategoryUseCase.UseCase;
+  @Inject(ListCategoriesUseCase.UseCase)
+  private readonly listUseCase: ListCategoriesUseCase.UseCase;
+  @Inject(GetCategoryUseCase.UseCase)
+  private readonly getUseCase: GetCategoryUseCase.UseCase;
+  @Inject(DeleteCategoryUseCase.UseCase)
+  private readonly deleteUseCase: DeleteCategoryUseCase.UseCase;
+  @Inject(UpdateCategoryUseCase.UseCase)
+  private readonly updateUseCase: UpdateCategoryUseCase.UseCase;
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.createUseCase.execute(createCategoryDto);
+  }
+
+  @Get()
+  search(@Query() searchParams: SearchCategoryDto) {
+    return this.listUseCase.execute(searchParams);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.getUseCase.execute({ id });
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.updateUseCase.execute({ id, ...updateCategoryDto });
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.deleteUseCase.execute({ id });
+  }
+}
